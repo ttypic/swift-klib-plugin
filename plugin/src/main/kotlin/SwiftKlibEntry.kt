8840bee8889b7a62a9bd662ef56a59b704553c4b
evgeny <khokhlov.e.n@gmail.com>
2022-02-15 11:14:22 +0300

    chore: clean up

--- plugin/src/main/kotlin/SwiftKlibEntry.kt
+++ plugin/src/main/kotlin/SwiftKlibEntry.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 
 abstract class SwiftKlibEntry @Inject constructor(val name: String) {
     abstract val src: Property<String>
-    abstract var targets: String?
+    abstract var targets: Property<List<String>>
 }
--- plugin/src/main/kotlin/SwiftKlibPlugin.kt
+++ plugin/src/main/kotlin/SwiftKlibPlugin.kt
@@ -3,18 +3,14 @@ package com.ttypic.swiftklib.gradle
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.model.ObjectFactory
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMultiplatformPlugin
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithSimulatorTests
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 
 
 class SwiftKlibPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
-        // project.plugins.apply(KotlinMultiplatformPlugin::class.java)
-
         val objects: ObjectFactory = project.objects
 
         val swiftKlibEntries: NamedDomainObjectContainer<SwiftKlibEntry> =
@@ -28,28 +24,8 @@ class SwiftKlibPlugin : Plugin<Project> {
 
         swiftKlibEntries.all { entry ->
             val name: String = entry.name
-            println("\n\n\n\n $name - ${entry.targets} \n\n\n\n")
-            val nativeTarget = kotlinMultiplatformExtension.targets.getByName(entry.targets!!) as KotlinNativeTargetWithSimulatorTests
-            nativeTarget.compilations.getByName("main").cinterops.getByName("SwiftChachaPoly") { settings ->
-                println("\n\n\n\n${settings.interopProcessingTaskName}\n\n\n\n\n\n\n")
-            }
-            tasks.withType(KotlinNativeCompile::class.java).de
-//            entry.targets.get().forEach {
-//                val nativeTarget = kotlinMultiplatformExtension.targets.getByName(it) as KotlinNativeTargetWithSimulatorTests
-//                nativeTarget.compilations.getByName("main").cinterops.getByName("SwiftChachaPoly") { settings ->
-//                    println("\n\n\n\n${settings.interopProcessingTaskName}\n\n\n\n\n\n\n")
-//                    project.tasks.register(
-//                        "tempTask",
-//                        Task::class.java
-//                    ) { task ->
-//                        tasks.findByPath(settings.interopProcessingTaskName)?.dependsOn(task)
-//                        println("\n\n\n\nI am alive\n\n\n\n\n\n\n")
-//                    }
-//                    // tasks.withType(CInteropProcess::class.java)
-//                    // tasks.withType(KotlinNativeCompile::class.java)
-//                }
-//            }
-
+            // register task for all targets
+            // add dependencies
         }
     }
 }
