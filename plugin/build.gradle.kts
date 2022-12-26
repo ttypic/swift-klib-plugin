5d37373fbf8bcdce7a17bcc07754f8359fa3dd27
evgeny <khokhlov.e.n@gmail.com>
2022-12-26 16:27:19 +0300

    chore: bump dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,21 +1,28 @@
 plugins {
     id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "1.1.0"
     kotlin("jvm") version "1.7.22"
 }
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    runtimeOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
 }
 
+version = "0.0.1"
+group = "io.github.ttypic"
+
 gradlePlugin {
+    website.set("https://github.com/ttypic/swift-klib-plugin")
+    vcsUrl.set("https://github.com/ttypic/swift-klib-plugin")
+
     plugins {
         create("swiftklib") {
             id = "io.github.ttypic.swiftklib"
             displayName = "SwiftKlib Gradle Plugin"
-            description = "SwiftKlib Gradle Plugin"
+            description = "Gradle Plugin to inject Swift-code for Kotlin Multiplatform iOS target"
             implementationClass = "io.github.ttypic.swiftklib.gradle.SwiftKlibPlugin"
+            tags.set(listOf("kotlin-multiplatform", "swift"))
         }
     }
 }
