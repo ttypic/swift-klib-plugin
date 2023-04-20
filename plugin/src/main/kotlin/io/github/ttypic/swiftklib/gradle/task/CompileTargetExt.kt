68d4995c7c5b7a912b8a3258a4d8e5c682efc035
evgeny <khokhlov.e.n@gmail.com>
2023-04-21 00:26:02 +0300

    WIP: add other Apple platforms (tvOS, macOS, watchOS)

--- plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/task/CompileTargetExt.kt
+++ plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/task/CompileTargetExt.kt
@@ -6,12 +6,28 @@ internal fun CompileTarget.os() = when(this) {
     CompileTarget.iosX64 -> "iphonesimulator"
     CompileTarget.iosArm64 -> "iphoneos"
     CompileTarget.iosSimulatorArm64 -> "iphonesimulator"
+    CompileTarget.watchosX64 -> "watchsimulator"
+    CompileTarget.watchosArm64 -> "watchos"
+    CompileTarget.watchosSimulatorArm64 -> "watchsimulator"
+    CompileTarget.tvosX64 -> "appletvsimulator"
+    CompileTarget.tvosArm64 -> "appletvos"
+    CompileTarget.tvosSimulatorArm64 -> "appletvsimulator"
+    CompileTarget.macosX64 -> "macosx"
+    CompileTarget.macosArm64 -> "macosx"
 }
 
 internal fun CompileTarget.arch() = when(this) {
     CompileTarget.iosX64 -> "x86_64"
     CompileTarget.iosArm64 -> "arm64"
     CompileTarget.iosSimulatorArm64 -> "arm64"
+    CompileTarget.watchosX64 -> "x86_64"
+    CompileTarget.watchosArm64 -> "arm64"
+    CompileTarget.watchosSimulatorArm64 -> "arm64"
+    CompileTarget.tvosX64 -> "x86_64"
+    CompileTarget.tvosArm64 -> "arm64"
+    CompileTarget.tvosSimulatorArm64 -> "arm64"
+    CompileTarget.macosX64 -> "x86_64"
+    CompileTarget.macosArm64 -> "arm64"
 }
 internal fun CompileTarget.simulatorSuffix() = when(this) {
     CompileTarget.iosX64 -> "-simulator"
