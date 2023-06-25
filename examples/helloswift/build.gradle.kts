53e190868ce35ee6079266399c05e5481b216871
evgeny <khokhlov.e.n@gmail.com>
2023-06-25 19:49:05 +0300

    chore: add more archs to helloswift example

--- examples/helloswift/build.gradle.kts
+++ examples/helloswift/build.gradle.kts
@@ -8,6 +8,14 @@ kotlin {
         iosX64(),
         iosArm64(),
         iosSimulatorArm64(),
+        tvosX64(),
+        tvosArm64(),
+        tvosSimulatorArm64(),
+        watchosX64(),
+        watchosArm64(),
+        watchosSimulatorArm64(),
+        macosArm64(),
+        macosX64(),
     ).forEach {
         it.compilations {
             val main by getting {
