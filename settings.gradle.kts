de5200208ba888c232e73b2ab92cbc8d02493b55
Ilya Gulya <ilya@gulya.me>
2024-06-05 18:34:20 +0500

    Move repositories to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,14 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
 rootProject.name = "SwiftKlib"
 include(":plugin")
