81bdb72a821fe2bc50fc85116ab3476e95ba687f
frankois <dabonot.francois@gmail.com>
2024-11-06 09:13:32 +0100

    Change ExperimentalSwiftklibApi level
    It needs to be set at Warning Level or it will be considered as a Error.

--- plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/api/ExperimentalSwiftklibApi.kt
+++ plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/api/ExperimentalSwiftklibApi.kt
@@ -1,6 +1,9 @@
 package io.github.ttypic.swiftklib.gradle.api
 
-@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
+@RequiresOptIn(
+    message = "This API is experimental. It may be changed in the future without notice.",
+    level = RequiresOptIn.Level.WARNING
+)
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
 annotation class ExperimentalSwiftklibApi {
